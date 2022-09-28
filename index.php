335e8bede294ba0566688df8234c1309f224c1d1
dianajacinto <114600688+dianajacinto@users.noreply.github.com>
2022-09-29 00:41:06 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Activity 1</title>
+</head>
+<body>
+<p>Diana Rose Tobias Jacinto </p> <br>
+<p>BSIT-4A</p><br>
+<p>System Integration and Arachitecture</p><br>
+</body>
+</html>
